2b5357664eeb1c75b0c6a553503f1048ee0862d0
league <league@adminisorsimac3.attlocal.net>
2025-02-07 16:55:26 -0800

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- src/_04_gui_from_scratch/_2_jack_in_the_box/JackInTheBox.java
+++ src/_04_gui_from_scratch/_2_jack_in_the_box/JackInTheBox.java
@@ -8,8 +8,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JApplet;
 
 import java.applet.AudioClip;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 
 import javax.swing.JFrame;
@@ -19,106 +19,83 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JLabel;
-public class JackInTheBox implements MouseListener {
+
+public class JackInTheBox implements ActionListener{
 	JButton button = new JButton();
 	JFrame frame = new JFrame();
 	Random rand = new Random();
-	JPanel panel = new JPanel();
-	;
+	JPanel panel = new JPanel();;
 	int random = rand.nextInt(4);
+
 	public void running() {
 		button.setText("click meh");
-		frame.setVisible(true);
-		button.setVisible(true);
-		button.addActionListener(this);
-		frame.add(button);
+		panel.add(button);
 		frame.add(panel);
-		if(random == 4)
-		{
-			createLabelImage("jackInTheBox.png");
-			showPicture("jackInTheBox.png");
-			JOptionPane.showMessageDialog(panel, "BOOOOO!");
-			playSound("homer-woohoo.wav");
-		}
-		else
-		{
-			
+		frame.setVisible(true);
+		frame.setSize(800,600);
+		button.addActionListener(this);
+		if (random == 4) {
+
+		} else {
 		}
 	}
 
-	
-private void showPicture(String fileName) {
-	     try {
-	    	 JLabel imageLabel = new JLabel();
-	          imageLabel = createLabelImage(fileName);
-	          
-	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	          frame.add(imageLabel);
-	          frame.setVisible(true);
-	          frame.pack();
-	     } catch (Exception e) {
-	          e.printStackTrace();
-	     }
+	private void showPicture(String fileName) {
+		try {
+			JLabel imageLabel = new JLabel();
+			imageLabel = createLabelImage(fileName);
+
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private JLabel createLabelImage(String fileName) {
-	     try {
-	          URL imageURL = getClass().getResource(fileName);
-	          if (imageURL == null) {
-	               System.err.println("Could not find image " + fileName);
-	               return new JLabel();
-	          } else {
-	               Icon icon = new ImageIcon();
-	               JLabel imageLabel = new JLabel(icon);
-	               return imageLabel;
-	          }
-	     } catch (Exception e) {
-	          System.err.println("Could not find image " + fileName);
-	          return new JLabel();
-	     }
+		try {
+			URL imageURL = getClass().getResource(fileName);
+			if (imageURL == null) {
+				System.err.println("Could not find image " + fileName);
+				return new JLabel();
+			} else {
+				Icon icon = new ImageIcon();
+				JLabel imageLabel = new JLabel(icon);
+				return imageLabel;
+			}
+		} catch (Exception e) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
 	}
+
 	private void playSound(String soundFile) {
-	     try {
-	          AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
-	          sound.play();
-	     } catch (Exception e) {
-	          e.printStackTrace();
-	     }
+		try {
+			AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+			sound.play();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
+	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		JButton ButtonPressed = (JButton) arg0.getSource();
-		if(ButtonPressed.equals(button))
-		{
-			
-			if(random == 4)
-			{
+		if (ButtonPressed.equals(button)) {
+			random = rand.nextInt(4);
+			if (random == 4) {
 				JOptionPane.showMessageDialog(panel, "...");
-			}
-			else
-			{
+				createLabelImage("jackInTheBox.png");
+				showPicture("jackInTheBox.png");
+				JOptionPane.showMessageDialog(panel, "BOOOOO!");
+				playSound("homer-woohoo.wav");
+			} else {
 				JOptionPane.showMessageDialog(panel, "don't worry, you're safe, for now...");
 			}
 		}
 	}
-	@Override
-    public void mouseEntered(MouseEvent e) {
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-	
-	}
-		
 
 }
--- src/_04_gui_from_scratch/_3_sound_effects_machine/SoundEffectsMachine.java
+++ src/_04_gui_from_scratch/_3_sound_effects_machine/SoundEffectsMachine.java
@@ -1,5 +1,35 @@
 package _04_gui_from_scratch._3_sound_effects_machine;
 
-public class SoundEffectsMachine {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class SoundEffectsMachine implements ActionListener {
+	JButton button1 = new JButton("");
+	JButton button2 = new JButton("");
+	JButton button3 = new JButton("");
+	JFrame frame = new JFrame("");
+	JPanel panel = new JPanel();
+	public void running() {
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+	}
+
+
+public void actionPerformed(ActionEvent arg0)
+{
+	JButton buttonPressed = (JButton) arg0.getSource();
+
+}
 }
